09bcc2c8d0f8033e92e340cc1a21bcfc6d6843de
Isaev Ilya <isaev.ameninc@gmail.com>
2019-10-22 22:37:58 +0300

    Modified TrafficSim

--- include/TrafficSim/TrafficSim.hpp
+++ include/TrafficSim/TrafficSim.hpp
@@ -1,4 +1,12 @@
+#pragma once
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <Engine/Camera.hpp>
+#include <Engine/ShaderLoader.hpp>
+#include <Engine/LightRenderer.hpp>
+
 
 void renderScene();
+
+void initApplication();
--- src/TrafficSim/main.cpp
+++ src/TrafficSim/main.cpp
@@ -2,11 +2,18 @@
 
 enum{screenWidth = 800, screenHeight = 600};
 
+extern Camera* camera;
+extern LightRenderer* light;
+
 int main(int argc, char** argv) {
   glfwInit();
-  GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Traffic Simulation", nullptr, nullptr);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Traffic Simulation", NULL, NULL);
   glfwMakeContextCurrent(window);
   glewInit();
+  initApplication();
   while(!glfwWindowShouldClose(window)) {
     // Some render stuff
     renderScene();
@@ -15,5 +22,7 @@ int main(int argc, char** argv) {
     glfwPollEvents();
   }
   glfwTerminate();
+  delete camera;
+  delete light;
   return 0;
 }
