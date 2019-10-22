07c1da15808234d6abf667d51ac01df158765928
Vlad Nazarov <vladnazarov2@gmail.com>
2019-10-22 13:44:05 +0300

    Added window creation

---
+++ src/SmartHouse.cpp
@@ -0,0 +1,8 @@
+#include "SmartHouse.hpp"
+
+void renderScene(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(1.0, 1.0, 0.0, 1.0); // clear Yellow
+
+    // draw objects here
+}
--- src/mainSmartHouse.cpp
+++ src/mainSmartHouse.cpp
@@ -1,3 +1,18 @@
-int main(){
+#include "SmartHouse.hpp"
+
+int main(int argc, char **argv){
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, 
+    "SmartHouse", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glewInit();
+    while(!glfwWindowShouldClose(window)){
+        renderScene();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
