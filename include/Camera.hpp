ad0d3381d9bb775cb54d02cc17c944ce8251296a
Vlad Nazarov <vladnazarov2@gmail.com>
2019-10-22 15:04:21 +0300

    Add Camera and Mesh classes

---
+++ include/Camera.hpp
@@ -0,0 +1,19 @@
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera{
+    public:
+        Camera(GLfloat FOV, GLfloat width, GLfloat height, GLfloat nearPlane,
+            GLfloat farPlane, glm::vec3 camPos);
+        ~Camera();
+
+        glm::mat4 getViewMatrix();
+        glm::mat4 getProjectionMatrix();
+        glm::vec3 getCameraPosition();
+
+    private:
+        glm::mat4 viewMatrix;
+        glm::mat4 projectionMatrix;
+        glm::vec3 cameraPos;
+};
