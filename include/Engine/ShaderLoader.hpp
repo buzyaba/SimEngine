ce5f46c89cfd0ed3b7a22a55776642265d12fd5d
Isaev Ilya <isaev.ameninc@gmail.com>
2019-10-22 20:06:46 +0300

    Added ShaderLoader

---
+++ include/Engine/ShaderLoader.hpp
@@ -0,0 +1,13 @@
+#include <GL/glew.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+class ShaderLoader {
+    public:
+        GLuint CreateProgram(const std::string vertexShaderFileName, const std::string fragmentShaderFileName);
+    private:
+        std::string readShader(const std::string filename);
+        GLuint createShader(GLenum shaderType, std::string source, const std::string shaderName);
+};
--- src/TrafficSim/main.cpp
+++ src/TrafficSim/main.cpp
@@ -1,55 +1,19 @@
-#include <glm/glm.hpp>
-#include <GLFW/glfw3.h>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/transform.hpp>
-#include <iostream>
 #include <TrafficSim/TrafficSim.hpp>
 
-int main() {
-  if(!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
-  }
+enum{screenWidth = 800, screenHeight = 600};
 
-  glfwDefaultWindowHints();
-
-  GLFWwindow* window = glfwCreateWindow(300, 300, "Red Triangle",
-                                        nullptr, nullptr);
-  if(window == nullptr) {
-    std::cerr << "Failed to open GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-
+int main(int argc, char** argv) {
+  glfwInit();
+  GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Traffic Simulation", nullptr, nullptr);
   glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
-  glfwShowWindow(window);
-
-  // Важно! Не эквивалентно glEnable(GL_DEPTH_TEST | GL_DOUBLEBUFFER)
-  glEnable(GL_DEPTH_TEST);
-  glEnable(GL_DOUBLEBUFFER);
-  glDepthFunc(GL_LESS);
-
-  glClearColor(0, 0, 0, 1);
-
-  glm::mat4 m = glm::perspective(45.0f, 4.0f / 3.0f, 1.0f, 100.0f);
-  glMatrixMode(GL_PROJECTION);
-  glLoadMatrixf(glm::value_ptr(m));
-
-  while(glfwWindowShouldClose(window) == GL_FALSE) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor4f(1, 0, 0, 1);
-    glBegin(GL_TRIANGLES);
-    glVertex3f(   0,  0.5, -5);
-    glVertex3f( 0.5, -0.5, -5);
-    glVertex3f(-0.5, -0.5, -5);
-    glEnd();
+  glewInit();
+  while(!glfwWindowShouldClose(window)) {
+    // Some render stuff
+    renderScene();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-
-  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
