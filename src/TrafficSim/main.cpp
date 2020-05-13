09865f74844ba2afbc14eeeab416c87b2b75a45c
buzyaba <isaev.ameninc@gmail.com>
2020-05-13 21:53:13 +0300

    Linker

--- src/TrafficSim/main.cpp
+++ src/TrafficSim/main.cpp
@@ -1,34 +1,54 @@
 #include <chrono>
 #include <algorithm>
 #include <vector>
-#include <Engine/IsometricView.hpp>
 #include <Core/WorkManager.h>
 
-void initApplication();
-void myTickCallback(btDynamicsWorld *dynamicsWorld, btScalar timeStep);
-
-TWorkManager* workManager;
-WindowManager* window;
-
 int main(int argc, char** argv) {
-	window = new IsometricView(800, 600, "TrafficSim");
-	auto cwd = Renderer::getCWD();
-    auto c_cwd = cwd;
-    std::transform(c_cwd.begin(), c_cwd.end(), c_cwd.begin(), toupper);
-    auto i = c_cwd.find("SIMENGINE");
-	workManager = new TWorkManager(window, 1, cwd.substr(0,i+9) + "/ConfigFiles/conf_traffic.xml",
-		cwd.substr(0,i+9) + "/ConfigFiles/Street.xml");
-	workManager->InitDraw();
-	workManager->Start();
-	delete workManager;
-	return 0;
-}
+	// window = new IsometricView(800, 600, "TrafficSim");
+	// auto cwd = Renderer::getCWD();
+    // auto c_cwd = cwd;
+    // std::transform(c_cwd.begin(), c_cwd.end(), c_cwd.begin(), toupper);
+    // auto i = c_cwd.find("SIMENGINE");
+	// workManager = new TWorkManager(window, 1, cwd.substr(0,i+9) + "/ConfigFiles/conf_traffic.xml",
+	// 	cwd.substr(0,i+9) + "/ConfigFiles/Street.xml");
+	// workManager->InitDraw();
+	// workManager->Start();
+	// delete workManager;
+	// return 0;
+	  =try
+  {
+    GlobalParameters.LoadConsoleParameters(argc, argv);
+  }
+  catch (...)
+  {
+    std::cout << "Error Load Parameters!!!" << std::endl;
+  }
+  TWorkManager* workManager;
 
-void initApplication() {
-	Renderer::getDynamicsWorld()->setInternalTickCallback(myTickCallback);
-}
+  try
+  {
+    workManager = new TWorkManager(GlobalParameters);
+    workManager->InitDraw();
+  }
+  catch (...)
+  {
+    std::cout << "Error Create WorkManager!!!" << std::endl;
+  }
 
-void myTickCallback(btDynamicsWorld *_dynamicsWorld, btScalar
-    timeStep) {
-
+  try
+  {
+    int _enableVisualisation = 1;
+#ifdef USE_OpenGL
+    _enableVisualisation = 1;
+#else
+    _enableVisualisation = 0;
+#endif
+    workManager->Start(_enableVisualisation);
+  }
+  catch (...)
+  {
+    std::cout << "Error Start WorkManager!!!" << std::endl;
+  }
+  delete workManager;
+  return 0;
 }
