4c0fba62b3b72fa50798fa909765fbbb5ba657fb
LonelyBaka <vladnazarov2@gmail.com>
2020-10-10 00:50:42 +0300

    Fix windows build

--- GenerateMinimumProject.bat
+++ GenerateMinimumProject.bat
@@ -1,6 +1,6 @@
 git submodule update --init --recursive
 if not exist _buildMP mkdir _buildMP
 cd _buildMP
-cmake -Dglew-cmake_BUILD_SHARED=OFF -Dglew-cmake_BUILD_SINGLE_CONTEXT=OFF -DBUILD_TESTS=ON -DBUILD_PLOTTER=OFF -DUSE_OPENGL=OFF -DBUILD_DLL=ON ..
+cmake -DBUILD_TESTS=ON -DUSE_OPENGL=OFF ..
 cd ..
 pause
