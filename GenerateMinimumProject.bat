cf9f47938c78dcd51524440cf006c5f1fc0dcf0b
buzyaba <isaev.ameninc@gmail.com>
2020-10-10 14:12:13 +0300

    Add executable permissions

--- GenerateMinimumProject.bat
+++
@@ -1,6 +0,0 @@
-git submodule update --init --recursive
-if not exist _buildMP mkdir _buildMP
-cd _buildMP
-cmake -DBUILD_TESTS=ON -DUSE_OPENGL=OFF ..
-cd ..
-pause
--- GenerateProject.bat
+++
@@ -1,6 +0,0 @@
-git submodule update --init --recursive
-if not exist _build mkdir _build
-cd _build
-cmake ..
-cd ..
-pause
