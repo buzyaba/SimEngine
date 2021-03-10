18e11efcf5402bbfbfb50bdbc36656253170b5b9
LonelyBaka <vladnazarov2@gmail.com>
2021-03-10 22:40:37 +0300

    Add Object Tests

--- Tests/src/ObjectTest.cpp
+++ Tests/src/ObjectTest.cpp
@@ -1,24 +1,78 @@
 #include "gtest/gtest.h"
-// #include "Core/Object.h"
-// #include "Core/Scene.h"
-// #include "Core/ObjectOfObservation.h"
-// #include "Core/Sensor.h"
-// #include "Core/Properties.h"
-// #include "Core/Actuator.h"
-// #include "Core/SmartThing.h"
-// #include "Core/DataStore.h"
-// #include "Core/WorkManager.h"
-// #include "Core/DataPacket.h"
-// #include "Core/ManagementProgram.h"
-// #include "Core/ExternalActionSchedule.h"
+#include "SimEngine/Object.h"
 
-// TEST(ObjectTest, CanCreate)
-// {
-//   //virtual void SetProperty(IProperties& property) = 0;
-//   //virtual std::vector<IProperties*> GetProperties() = 0;
-//   //virtual IProperties& GetProperty(std::string _name = "") = 0;
+TEST(ObjectTest, Can_Create_Instance) {
+    ASSERT_NO_THROW(TObject("ourBestObject"));
+};
 
-//   //virtual std::string GetName() = 0;
-//   //virtual void SetName(std::string _name) = 0;
-//   // EXPECT_EQ(4, 2 + 2);
-// };
+TEST(ObjectTest, Get_Object_Name) {
+    TObject obj("ourBestObject");
+    ASSERT_EQ(obj.GetName(), "ourBestObject");
+}
+
+TEST(ObjectTest, Set_Object_Name) {
+    TObject obj("ourBestObject");
+    obj.SetName("AwesomeObject");
+    ASSERT_EQ(obj.GetName(), "AwesomeObject");
+}
+
+TEST(ObjectTest, Get_Class_Name) {
+    TObject obj("ourBestObject");
+    ASSERT_EQ(obj.ClassName(), "TObject");
+}
+
+TEST(ObjectTest, Add_Property) {
+    TObject obj("ourBestObject");
+    auto property =
+        TProperties(std::map<std::string, double>{ { "Property", 50 } }, true, "Property");
+    obj.AddProperty(property);
+    double val = obj.GetProperty("Property").GetValue("Property");
+    ASSERT_DOUBLE_EQ(50, val);
+}
+
+TEST(ObjectTest, Set_Property_By_Reference) {
+    TObject obj("ourBestObject");
+    auto property =
+        TProperties(std::map<std::string, double>{ { "Property", 50 } }, true, "Property");
+    obj.AddProperty(property);
+
+    auto new_property =
+        TProperties(std::map<std::string, double>{ { "Property", 100 } }, true, "Property");
+
+    obj.SetProperty(new_property);
+
+    double val = obj.GetProperty("Property").GetValue("Property");
+    ASSERT_DOUBLE_EQ(100, val);
+}
+
+TEST(ObjectTest, Set_Property_By_Name) {
+    TObject obj("ourBestObject");
+    auto property =
+        TProperties(std::map<std::string, double>{ { "Property", 50 } }, true, "Property");
+    obj.AddProperty(property);
+
+    auto new_property =
+        TProperties(std::map<std::string, double>{ { "Property", 100 } }, true, "Property");
+
+    obj.SetProperty(std::map<std::string, double>{ { "Property", 100 }}, "Property");
+
+    double val = obj.GetProperty("Property").GetValue("Property");
+    ASSERT_DOUBLE_EQ(100, val);
+}
+
+TEST(ObjectTest, Get_All_Properties) {
+    TObject obj("ourBestObject");
+    auto property1 =
+        TProperties(std::map<std::string, double>{ { "Property1", 50 } }, true, "Property1");
+    obj.AddProperty(property1);
+
+    auto property2 =
+        TProperties(std::map<std::string, double>{ { "Property2", 100 } }, true, "Property2");
+    obj.AddProperty(property2);
+
+    auto property3 =
+        TProperties(std::map<std::string, double>{ { "Property3", 100 } }, true, "Property3");
+    obj.AddProperty(property3);
+
+    ASSERT_DOUBLE_EQ(obj.GetProperties().size(), 3);
+}
