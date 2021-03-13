f188b6bba6af414cef2b45435b4fb17de6eb7c6c
Ilya Isaev <ilya.isaev@intel.com>
2021-03-13 17:48:20 +0300

    ExternalAction and ThingsSchedule tests are added

--- Tests/src/ExternalActionTest.cpp
+++ Tests/src/ExternalActionTest.cpp
@@ -4,13 +4,6 @@
 #include "SimEngine/ObjectOfObservation.h"
 #include "Core/ExternalActionSchedule.h"
 
-// TEST(ExternalActionScheduleTest, CanCreate)
-// {
-//   //virtual IProperties& GetObjectProperties(std::string name) = 0;
-//   //virtual IProperties& GetObjectProperties(const IObject& object) = 0;
-//   // EXPECT_EQ(4, 2 + 2);
-// };
-
 class DummyObject : public TObjectOfObservation {
 public:
     DummyObject(std::string name) : TObjectOfObservation(name) {
@@ -21,7 +14,49 @@ public:
 
 TEST(ExternalActionTest, Can_Create) {
     std::vector<TObjectOfObservation*> allObjects(15);
-    std::for_each(allObjects.begin(), allObjects.end(), [](auto& elem) {elem = new DummyObject("dummy");} );
+    std::generate(allObjects.begin(), allObjects.end(), [] {static int a = 1; return new DummyObject("DummyObject"+std::to_string(a++));} );
 
-    ASSERT_NO_THROW(TExternalActionSchedule(allObjects, getPath("/Tests/src/actions.xml")));
+    ASSERT_NO_THROW(TExternalActionSchedule(allObjects, getPath("/Tests/common/actions.xml")));
+}
+
+TEST(ExternalActionTest, GetObjectProperties_string) {
+    std::vector<TObjectOfObservation*> allObjects(1);
+    allObjects[0] = new DummyObject("DummyObject1");
+
+    TExternalActionSchedule schedule(allObjects, getPath("/Tests/common/actions.xml"));
+    auto property = schedule.GetObjectProperties("DummyObject1", 0ull);
+    ASSERT_DOUBLE_EQ(property["Property"]->GetValue("Property"), 0.0);
+
+    property = schedule.GetObjectProperties("DummyObject1", 1000ull);
+    ASSERT_DOUBLE_EQ(property["Property"]->GetValue("Property"), 1.0);
+}
+
+TEST(ExternalActionTest, GetObjectProperties_reference) {
+    std::vector<TObjectOfObservation*> allObjects(1);
+    allObjects[0] = new DummyObject("DummyObject1");
+
+    TExternalActionSchedule schedule(allObjects, getPath("/Tests/common/actions.xml"));
+    auto property = schedule.GetObjectProperties(*allObjects[0], 0ull);
+    ASSERT_DOUBLE_EQ(property["Property"]->GetValue("Property"), 0.0);
+
+    property = schedule.GetObjectProperties(*allObjects[0], 1000ull);
+    ASSERT_DOUBLE_EQ(property["Property"]->GetValue("Property"), 1.0);
+}
+
+TEST(ExternalActionTest, UpdateObjectProperties) {
+    std::vector<TObjectOfObservation*> allObjects(15);
+    std::generate(allObjects.begin(), allObjects.end(), [] {
+        static int a = 1; 
+        return new DummyObject("DummyObject"+std::to_string(a++));
+    } );
+
+    TExternalActionSchedule schedule(allObjects, getPath("/Tests/common/actions.xml"));
+    schedule.UpdateObjectsProperties(0ull);
+    ASSERT_DOUBLE_EQ(allObjects[0]->GetProperty("Property").GetValue("Property"), 0.0);
+
+    schedule.UpdateObjectsProperties(500ull);
+    ASSERT_DOUBLE_EQ(allObjects[0]->GetProperty("Property").GetValue("Property"), 0.0);
+
+    schedule.UpdateObjectsProperties(1000ull);
+    ASSERT_DOUBLE_EQ(allObjects[0]->GetProperty("Property").GetValue("Property"), 1.0);
 }
---
+++ Tests/src/SmartThingScheduleTest.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include "gtest/gtest.h"
+#include "SimEngine/common.h"
+#include "SimEngine/SmartThing.h"
+#include "SimEngine/SmartThingSchedule.hpp"
+
+class DummyActuator: public TActuator {
+public:
+  DummyActuator(std::string _name) : TActuator(_name) {}
+
+  virtual void SetDataPacket(TDataPacket& packet)
+  {
+    for (int i = 0; i < objects.size(); i++)
+    {
+      if (objects[i] != nullptr)
+      {
+        switch (static_cast<int>(packet.GetData<double>()[i])) {
+          case 0:
+            objects[i]->SetProperty({{"Property", 0}}, "Property");
+            break;
+          case 1:
+            objects[i]->SetProperty({{"Property", 1}}, "Property");
+            break;
+          case 2:
+            objects[i]->SetProperty({{"Property", 2}}, "Property");
+            break;
+        }
+      }
+    }
+  }
+};
+
+class DummySensor: public TSensor {
+public:
+    DummySensor(std::string _name) : TSensor(_name) {}
+};
+
+class DummyThing : public TSmartThing {
+public:
+    DummyThing(std::string name) : TSmartThing(name, { new DummySensor("DummySensor") }, { new DummyActuator("DummyActuator") }) {}
+    TSmartThing* Clone() { return new DummyThing("DummyThing"); };
+    std::string ClassName() override { return std::string("DummyThing"); }
+};
+
+class DummyObject : public TObjectOfObservation {
+public:
+    DummyObject(std::string name) : TObjectOfObservation(name) {
+        properties.insert(
+            {"Property", new TProperties({{"Property", 2}}, false, "Property")});
+    }
+};
+
+TEST(SmartThingScheduleTest, Can_Create) {
+    std::vector<TObjectOfObservation*> objects(15);
+    std::generate(objects.begin(), objects.end(), [] {static int a = 1; return new DummyObject("DummyObject"+std::to_string(a++));} );
+
+    std::vector<TSmartThing*> things(15);
+    std::generate(things.begin(), things.end(), [&] {
+        static int a = 1;
+        auto Thing = new DummyThing("DummyThing"+std::to_string(a));
+        Thing->AddObject(*objects[a-1]);
+        a++;
+        return Thing;
+    } );
+
+    ASSERT_NO_THROW(TSmartThingSchedule(things, getPath("/Tests/common/things.xml")));
+}
+
+TEST(SmartThingScheduleTest, UpdateThingsProperties) {
+    std::vector<TObjectOfObservation*> objects(15);
+    std::generate(objects.begin(), objects.end(), [] {static int a = 1; return new DummyObject("DummyObject"+std::to_string(a++));} );
+
+    std::vector<TSmartThing*> things(15);
+    std::generate(things.begin(), things.end(), [&] {
+        static int a = 1;
+        auto Thing = new DummyThing("DummyThing"+std::to_string(a));
+        Thing->AddObject(*objects[a-1]);
+        a++;
+        return Thing;
+    } );
+
+    TSmartThingSchedule thingsSchedule(things, getPath("/Tests/common/things.xml"));
+
+    thingsSchedule.UpdateThingsProperties(0ull);
+    ASSERT_DOUBLE_EQ(objects[0]->GetProperty("Property").GetValue("Property"), 0.0);
+
+    thingsSchedule.UpdateThingsProperties(500ull);
+    ASSERT_DOUBLE_EQ(objects[0]->GetProperty("Property").GetValue("Property"), 0.0);
+
+    thingsSchedule.UpdateThingsProperties(1000ull);
+    ASSERT_DOUBLE_EQ(objects[0]->GetProperty("Property").GetValue("Property"), 1.0);
+}
